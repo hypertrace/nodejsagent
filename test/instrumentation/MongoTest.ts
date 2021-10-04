737016e95b5ffb9cc18d7b6fb3d231b1843a295b
prodion23 <donniepropst@donnies-mbp.lan>
2021-10-04 18:43:01 -0400

    fix hanging mongo test

--- test/instrumentation/MongoTest.ts
+++ test/instrumentation/MongoTest.ts
@@ -4,36 +4,38 @@ const agentTestWrapper = AgentForTest.getInstance();
 agentTestWrapper.instrument()
 import * as mongodb from 'mongodb';
 import {trace, context} from "@opentelemetry/api";
+import {MongoClient} from "mongodb";
 
 describe('Mongo test', () => {
-    afterEach(() => {
+    let client : MongoClient
+    beforeEach(async () => {
+        const url = 'mongodb://hypertrace:hypertrace@localhost:27017';
+        let mClient = await MongoClient.connect(url)
+            .catch(err => { console.log(err); });
+        client = mClient as MongoClient
+    })
+    afterEach(async () => {
+        await (client as MongoClient).close()
         agentTestWrapper.stop()
     })
 
-    it('will capture a span for pool query', (done) => {
-        const url = 'mongodb://hypertrace:hypertrace@localhost:27017';
-        const client = new mongodb.MongoClient(url);
+    it('will capture a span for pool query', async () => {
         const span = trace.getTracer('default').startSpan('insertRootSpan');
-        context.with(trace.setSpan(context.active(), span), async () => {
-            const dbName = 'testMongo';
+        trace.setSpan(context.active(), span)
+        const dbName = 'testMongo';
 
-            await client.connect();
-            const db = client.db(dbName);
-            const collection = db.collection('documents');
-            await collection.insertMany([
-                {a: 1}, {a: 2}, {a: 3}
-            ])
-
-            let spans = agentTestWrapper.getSpans();
-            expect(spans.length).to.equal(1);
-            let spanAttrs = spans[0].attributes
-            expect(spanAttrs['db.system']).to.equal("mongodb")
-            expect(spanAttrs['db.name']).to.equal("testMongo")
-            expect(spanAttrs["db.mongodb.collection"]).to.equal("documents")
-            expect(spanAttrs["db.statement"]).to.equal("{\"a\":\"?\",\"_id\":\"?\"}")
-            expect(spans[0].name).to.equal("mongodb.insert")
-
-            done()
-        });
+         const db = (<MongoClient>client).db(dbName);
+        const collection = db.collection('documents');
+        await collection.insertMany([
+            {a: 1}, {a: 2}, {a: 3}
+        ])
+        let spans = agentTestWrapper.getSpans();
+        expect(spans.length).to.equal(1);
+        let spanAttrs = spans[0].attributes
+        expect(spanAttrs['db.system']).to.equal("mongodb")
+        expect(spanAttrs['db.name']).to.equal("testMongo")
+        expect(spanAttrs["db.mongodb.collection"]).to.equal("documents")
+        expect(spanAttrs["db.statement"]).to.equal("{\"a\":\"?\",\"_id\":\"?\"}")
+        expect(spans[0].name).to.equal("mongodb.insert")
     })
 })
