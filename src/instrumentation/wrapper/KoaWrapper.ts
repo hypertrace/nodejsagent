5d7df75bce7a90cc22d3b083dcc1ebebd1d8f635
prodion23 <donniepropst@donnies-mbp.lan>
2021-12-02 13:21:01 -0500

    Revert "update to use processableString instead of dataString for filter calls"
    This reverts commit 3a0125d1c2c9a42827090c7ab5f104ef2756b6a1.

--- src/instrumentation/wrapper/KoaWrapper.ts
+++ src/instrumentation/wrapper/KoaWrapper.ts
@@ -29,7 +29,7 @@ export function koaRequestCallback(context: any, span: any) {
     const filterResult = Registry.getInstance().applyFilters(span,
         context.originalUrl,
         reqHeaders,
-        bodyCapture.processableString(),
+        bodyCapture.dataString(),
         REQUEST_TYPE.HTTP )
     if(filterResult){
         context.throw(STATUS_CODE, MESSAGE);
