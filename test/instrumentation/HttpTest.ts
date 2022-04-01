00e6952473e63e1ae8dfd12afc3d4d5bcfe2b9d0
prodion23 <donniepropst@Donnies-MacBook-Pro.local>
2022-04-01 10:45:31 -0400

    add req body test + read

--- test/instrumentation/HttpTest.ts
+++ test/instrumentation/HttpTest.ts
@@ -8,9 +8,20 @@ import {httpRequest} from "./HttpRequest";
 
 describe('Agent tests', () => {
     const requestListener = function (req, res) {
-        res.setHeader("Content-Type", "application/json")
-        res.writeHead(200);
-        res.end(`{"test": "data"}`);
+        let body = ''
+        if(req.method == "POST") {
+            req.on('data', (data) => {body += data})
+            req.on('end', (data) => {
+                body += data
+                res.setHeader("Content-Type", "application/json")
+                res.writeHead(200);
+                res.end(`{"test post": "data"}`);
+            })
+        } else {
+            res.setHeader("Content-Type", "application/json")
+            res.writeHead(200);
+            res.end(`{"test": "data"}`);
+        }
     }
 
     let server = http.createServer(requestListener)
@@ -33,19 +44,19 @@ describe('Agent tests', () => {
         const response = await httpRequest.get('http://localhost:8000/')
         let spans = agentTestWrapper.getSpans()
         expect(spans.length).to.equal(2)
-        let requestSpanAttributes = spans[0].attributes
-        expect(requestSpanAttributes['http.method']).to.equal('GET')
-        expect(requestSpanAttributes['net.host.name']).to.equal('localhost')
-        expect(requestSpanAttributes['http.target']).to.equal('/')
-        expect(requestSpanAttributes['net.transport']).to.equal('ip_tcp')
-        expect(requestSpanAttributes['http.status_code']).to.equal(200)
-
-        let serverSpanAttributes = spans[1].attributes
+        let serverSpanAttributes = spans[0].attributes
         expect(serverSpanAttributes['http.method']).to.equal('GET')
-        expect(serverSpanAttributes['net.peer.name']).to.equal('localhost')
+        expect(serverSpanAttributes['net.host.name']).to.equal('localhost')
         expect(serverSpanAttributes['http.target']).to.equal('/')
         expect(serverSpanAttributes['net.transport']).to.equal('ip_tcp')
         expect(serverSpanAttributes['http.status_code']).to.equal(200)
+
+        let requestSpanAttributes = spans[1].attributes
+        expect(requestSpanAttributes['http.method']).to.equal('GET')
+        expect(requestSpanAttributes['net.peer.name']).to.equal('localhost')
+        expect(requestSpanAttributes['http.target']).to.equal('/')
+        expect(requestSpanAttributes['net.transport']).to.equal('ip_tcp')
+        expect(requestSpanAttributes['http.status_code']).to.equal(200)
         expect(spans[1].name).to.equal('HTTP GET')
     });
 
@@ -53,11 +64,11 @@ describe('Agent tests', () => {
         const response = await httpRequest.get('http://localhost:8000/')
         let spans = agentTestWrapper.getSpans()
         expect(spans.length).to.equal(2)
-        let requestSpanAttributes = spans[0].attributes
-        expect(requestSpanAttributes['http.response.body']).to.equal(`{"test": "data"}`)
-
-        let serverSpanAttributes = spans[1].attributes
+        let serverSpanAttributes = spans[0].attributes
         expect(serverSpanAttributes['http.response.body']).to.equal(`{"test": "data"}`)
+
+        let requestSpanAttributes = spans[1].attributes
+        expect(requestSpanAttributes['http.response.body']).to.equal(`{"test": "data"}`)
     });
 
     it('can capture request body data', async () => {
@@ -66,6 +77,7 @@ describe('Agent tests', () => {
                 port: 8000,
                 path: '/',
                 headers: {
+                    "Some-Header": "Foo-Bar",
                     "Content-Type": "application/json"
                 }
             },
@@ -73,11 +85,11 @@ describe('Agent tests', () => {
         )
         let spans = agentTestWrapper.getSpans()
         expect(spans.length).to.equal(2)
-        let requestSpanAttributes = spans[0].attributes
-        expect(requestSpanAttributes['http.response.body']).to.equal(`{"test": "data"}`)
+        let serverSpanAttributes = spans[0].attributes
+        expect(serverSpanAttributes['http.request.body']).to.equal(`{"data":"123"}`)
 
-        let serverSpanAttributes = spans[1].attributes
-        expect(serverSpanAttributes['http.response.body']).to.equal(`{"test": "data"}`)
+        let requestSpanAttributes = spans[1].attributes
+        expect(requestSpanAttributes['http.request.body']).to.equal(`{"data":"123"}`)
     });
 
 
