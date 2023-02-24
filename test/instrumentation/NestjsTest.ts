f9d47f80466363616074c9986b1db1545b525282
prodion23 <donnie@traceable.ai>
2023-02-24 11:36:52 -0500

    Add tests to verify net peer ip is captured, update grpc to capture peer ip & port

--- test/instrumentation/NestjsTest.ts
+++ test/instrumentation/NestjsTest.ts
@@ -152,6 +152,7 @@ if(!['v8', 'v10'].some((nodeVersion) => {process.version.startsWith(nodeVersion)
                 let spans = agentTestWrapper.getSpans()
                 expect(spans.length).to.equal(2)
                 let serverSpan = spans[0]
+                expect(serverSpan.attributes["net.peer.ip"]).to.exist
                 expect(serverSpan.attributes['http.status_code']).to.equal(403)
                 expect(serverSpan.attributes['http.status_text']).to.equal('FORBIDDEN')
 
