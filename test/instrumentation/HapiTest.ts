cb16c3a7769320fe85e64efca6d62620d873c8d9
Donald Propst <donnie@traceable.ai>
2024-03-08 11:12:55 -0500

    no rotuer span

--- test/instrumentation/HapiTest.ts
+++ test/instrumentation/HapiTest.ts
@@ -71,15 +71,15 @@ describe('Hapi tests', () => {
     it('can capture spans', async () => {
         const response = await httpRequest.get('http://localhost:8000/')
         let spans = agentTestWrapper.getSpans()
-        expect(spans.length).to.equal(3)
-        let serverSpanAttributes = spans[1].attributes
+        expect(spans.length).to.equal(2)
+        let serverSpanAttributes = spans[0].attributes
         expect(serverSpanAttributes['http.method']).to.equal('GET')
         expect(serverSpanAttributes['net.host.name']).to.equal('localhost')
         expect(serverSpanAttributes['http.target']).to.equal('/')
         expect(serverSpanAttributes['net.transport']).to.equal('ip_tcp')
         expect(serverSpanAttributes['http.status_code']).to.equal(200)
 
-        let requestSpanAttributes = spans[2].attributes
+        let requestSpanAttributes = spans[1].attributes
         expect(requestSpanAttributes['http.method']).to.equal('GET')
         expect(requestSpanAttributes['net.peer.name']).to.equal('localhost')
         expect(requestSpanAttributes['http.target']).to.equal('/')
