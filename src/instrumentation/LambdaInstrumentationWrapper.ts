f0cb57abc95af663fcfd0ff65d89f41c9cce7e2c
Donald Propst <donnie@traceable.ai>
2024-06-11 15:46:04 -0400

    Extra care for any errors parsing

--- src/instrumentation/LambdaInstrumentationWrapper.ts
+++ src/instrumentation/LambdaInstrumentationWrapper.ts
@@ -5,55 +5,60 @@ import {Config} from "../config/config";
 import {HttpInstrumentationWrapper} from "./HttpInstrumentationWrapper";
 
 export function LambdaRequestHook(span, {event, context}){
-    const headers = event['headers'];
-    const lambdaRequestContext = event['requestContext'];
+    try {
+        const headers = event['headers'];
+        const lambdaRequestContext = event['requestContext'];
 
-    if(!headers || !lambdaRequestContext){
-        logger.warn("Received unexpected lambda event that doesnt look like an ApiGateway event tracing will be skipped")
-        logger.warn("Is the event an ApiGateway event - enable debug logging to view original event")
-        logger.debug(event)
-        return
-    }
-
-    logger.debug('received lambda event')
-    logger.debug(event)
-
-    // event["version"] is not always present
-    // the request context will have an `http` field if v2
-    if(lambdaRequestContext['http']) {
-        let httpContext = lambdaRequestContext['http']
-        span.setAttribute(SemanticAttributes.HTTP_METHOD, httpContext['method'])
-        span.setAttribute(SemanticAttributes.HTTP_SCHEME, headers['x-forwarded-proto'])
-        span.setAttribute(SemanticAttributes.HTTP_TARGET, httpContext['path'])
-        span.setAttribute(SemanticAttributes.HTTP_HOST, headers['host'])
-    } else {
-        span.setAttribute(SemanticAttributes.HTTP_METHOD, event['httpMethod'])
-        let proto_header = headers['x-forwarded-proto'] || headers['X-Forwarded-Proto']
-        span.setAttribute(SemanticAttributes.HTTP_SCHEME, proto_header)
-        span.setAttribute(SemanticAttributes.HTTP_TARGET, event['path'])
-        span.setAttribute(SemanticAttributes.HTTP_HOST, lambdaRequestContext['domainName'])
-    }
-
-
-
-    let cookies = event['cookies']
-    if(cookies) {
-        span.setAttribute('http.request.header.cookie', cookies.join(';'))
-    }
-
-    // req header capture
-    if(<boolean>Config.getInstance().config.data_capture!.http_headers!.request!) {
-        for (const key in headers) {
-            span.setAttribute(`http.request.header.${key}`.toLowerCase(), <string>headers[key])
+        if(!headers || !lambdaRequestContext){
+            logger.warn("Received unexpected lambda event that doesn't look like an ApiGateway event tracing will be skipped")
+            logger.debug("Is the event an ApiGateway event - enable debug logging to view original event")
+            logger.debug(event)
+            return
         }
+
+        logger.debug('received lambda event')
+        logger.debug(event)
+
+        // event["version"] is not always present
+        // the request context will have an `http` field if v2
+        if(lambdaRequestContext['http']) {
+            let httpContext = lambdaRequestContext['http']
+            span.setAttribute(SemanticAttributes.HTTP_METHOD, httpContext['method'])
+            span.setAttribute(SemanticAttributes.HTTP_SCHEME, headers['x-forwarded-proto'])
+            span.setAttribute(SemanticAttributes.HTTP_TARGET, httpContext['path'])
+            span.setAttribute(SemanticAttributes.HTTP_HOST, headers['host'])
+        } else {
+            span.setAttribute(SemanticAttributes.HTTP_METHOD, event['httpMethod'])
+            let proto_header = headers['x-forwarded-proto'] || headers['X-Forwarded-Proto']
+            span.setAttribute(SemanticAttributes.HTTP_SCHEME, proto_header)
+            span.setAttribute(SemanticAttributes.HTTP_TARGET, event['path'])
+            span.setAttribute(SemanticAttributes.HTTP_HOST, lambdaRequestContext['domainName'])
+        }
+
+
+
+        let cookies = event['cookies']
+        if(cookies) {
+            span.setAttribute('http.request.header.cookie', cookies.join(';'))
+        }
+
+        // req header capture
+        if(<boolean>Config.getInstance().config.data_capture!.http_headers!.request!) {
+            for (const key in headers) {
+                span.setAttribute(`http.request.header.${key}`.toLowerCase(), <string>headers[key])
+            }
+        }
+        // req body capture
+        if (shouldCaptureBody(<boolean>Config.getInstance().config.data_capture!.http_body!.request!, headers)) {
+            let bodyCapture: BodyCapture = new BodyCapture(<number>Config.getInstance().config.data_capture!.body_max_size_bytes!,
+                <number>Config.getInstance().config.data_capture!.body_max_processing_size_bytes!)
+            bodyCapture.appendData(event['body'])
+            span.setAttribute('http.request.body', bodyCapture.dataString())
+        }
+    }catch(e){
+       logger.error("Error running LambdaRequestHook", e)
     }
-    // req body capture
-    if (shouldCaptureBody(<boolean>Config.getInstance().config.data_capture!.http_body!.request!, headers)) {
-        let bodyCapture: BodyCapture = new BodyCapture(<number>Config.getInstance().config.data_capture!.body_max_size_bytes!,
-            <number>Config.getInstance().config.data_capture!.body_max_processing_size_bytes!)
-        bodyCapture.appendData(event['body'])
-        span.setAttribute('http.request.body', bodyCapture.dataString())
-    }
+
 }
 
 export function LambdaResponseHook(span, {err, res}) : void {
